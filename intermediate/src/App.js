ba263d49f60d28aeee72ef98cdbb82d7c8e79b77
go5010 <gota.4305@gmail.com>
2024-01-03 15:32:33 +0900

    part12完了

--- intermediate/src/App.js
+++ intermediate/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./ChildArea";
 
 function App() {
@@ -12,13 +12,15 @@ function App() {
 
   const onClickOpen = () => setOpen(!open);
 
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
