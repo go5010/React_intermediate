ef1f1e2be395fefdd74dacecda1ac10e17a33f95
go5010 <gota.4305@gmail.com>
2024-01-03 12:54:49 +0900

    part8完了

--- intermediate/src/App.js
+++ intermediate/src/App.js
@@ -1,33 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { ChildArea } from "./ChildArea";
 
 function App() {
   console.log("App");
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 }
