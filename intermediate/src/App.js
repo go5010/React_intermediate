1303a43aa097d855771a1b44b9453ae95b3cb647
go5010 <gota.4305@gmail.com>
2024-01-03 12:25:05 +0900

    part7完了

---
+++ intermediate/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  console.log("App");
+  const [count, setCount] = useState(0);
+
+  const onClickCountUp = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+    </div>
+  );
+}
+
+export default App;
