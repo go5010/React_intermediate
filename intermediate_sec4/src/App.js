3b59b998618eea7f51017c0700f1a2fd15ee7e6c
go5010 <gota.4305@gmail.com>
2024-01-03 16:08:46 +0900

    part14完了

---
+++ intermediate_sec4/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { InlineStyle } from "./components/InlineStyle";
+
+function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+}
+
+export default App;
