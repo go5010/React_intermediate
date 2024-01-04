1ecb7e34a6b81a5bc5b60a8fd48cb5c984489a98
go5010 <gota.4305@gmail.com>
2024-01-04 18:13:56 +0900

    part15完了

--- intermediate_sec4/src/App.js
+++ intermediate_sec4/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
 
 function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
