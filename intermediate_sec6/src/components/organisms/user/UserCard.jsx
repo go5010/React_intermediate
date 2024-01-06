500666a797d7362e2277d92e603070c343eb6843
go5010 <gota.4305@gmail.com>
2024-01-06 18:09:45 +0900

    part33完了

---
+++ intermediate_sec6/src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>1111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-9999-999</dd>
+        <dt>会社名</dt>
+        <dd>abcd会社</dd>
+        <dt>WEB</dt>
+        <dd>httttt.com</dd>
+      </dl>
+    </div>
+  );
+};
