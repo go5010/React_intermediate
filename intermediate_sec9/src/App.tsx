51f51b3a507541f75feb403c6e21fd50cb7277fe
go5010 <gota.4305@gmail.com>
2024-01-07 23:36:19 +0900

    part50完了

--- intermediate_sec9/src/App.tsx
+++ intermediate_sec9/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
 
 function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
     </div>
   );
 }
