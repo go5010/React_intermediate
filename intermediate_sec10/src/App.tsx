a55eedde8d33580a8daac254c85596272d01bd20
go5010 <gota.4305@gmail.com>
2024-01-08 07:52:49 +0900

    part54完了

--- intermediate_sec10/src/App.tsx
+++ intermediate_sec10/src/App.tsx
@@ -1,15 +1,21 @@
 import axios from "axios";
 import "./App.css";
+import { useState } from "react";
+import { Todo } from "./Todo";
 
 function App() {
+  const [todos, setTodos] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
