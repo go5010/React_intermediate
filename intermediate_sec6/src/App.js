21e2b08cb3ccf2887e95ede6d0800bde457810d4
go5010 <gota.4305@gmail.com>
2024-01-07 15:51:43 +0900

    part36完了

--- intermediate_sec6/src/App.js
+++ intermediate_sec6/src/App.js
@@ -1,36 +1,8 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-
-const user = {
-  name: "jake",
-  image:
-    "https://images.unsplash.com/photo-1568572933382-74d440642117?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  email: "1245@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト株式会社",
-  },
-  website: "https://google.com",
-};
+import { Router } from "./components/router/Router";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
 
 export default App;
