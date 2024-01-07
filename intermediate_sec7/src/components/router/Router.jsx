ec5f52789fea38970fc1ba31ac2d821f374c46b0
go5010 <gota.4305@gmail.com>
2024-01-07 16:54:06 +0900

    part38完了

---
+++ intermediate_sec7/src/components/router/Router.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Top } from "../pages/Top";
+import { Users } from "../pages/Users";
+import { DefaultLayout } from "../templates/DefaultLayout";
+import { HeaderOnly } from "../templates/HeaderOnly";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <DefaultLayout>
+              <Top />
+            </DefaultLayout>
+          }
+        />
+
+        <Route
+          path="/users"
+          element={
+            <HeaderOnly>
+              <Users />
+            </HeaderOnly>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
