726f97858173a3a8b5b648e817e4d2318d8d19e5
go5010 <gota.4305@gmail.com>
2024-01-07 11:17:48 +0900

    part34完了

--- intermediate_sec6/src/App.js
+++ intermediate_sec6/src/App.js
@@ -5,6 +5,18 @@ import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 
+const user = {
+  name: "jake",
+  image:
+    "https://images.unsplash.com/photo-1568572933382-74d440642117?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  email: "1245@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社",
+  },
+  website: "https://google.com",
+};
+
 function App() {
   return (
     <div className="App">
@@ -12,7 +24,7 @@ function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
