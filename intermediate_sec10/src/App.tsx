071ed9e6c23f77d54e5a8e87125cb101d677e56e
go5010 <gota.4305@gmail.com>
2024-01-08 15:27:59 +0900

    part59完了

--- intermediate_sec10/src/App.tsx
+++ intermediate_sec10/src/App.tsx
@@ -3,6 +3,14 @@ import "./App.css";
 import { useState } from "react";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "jake",
+  hobbies: ["映画", "ゲーム"],
+};
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -15,6 +23,8 @@ function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
