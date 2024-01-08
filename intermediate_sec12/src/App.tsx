87995becd795dc34747fa5f08cbebdaf15f83108
go5010 <gota.4305@gmail.com>
2024-01-09 07:11:55 +0900

    part70完了

--- intermediate_sec12/src/App.tsx
+++ intermediate_sec12/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
+      <p>aaaaaaaaaaaaaaaaaaaaa</p>
     </ChakraProvider>
   );
 }
