0e10f420cc41239bae3c0882b091ad74e16a5327
go5010 <gota.4305@gmail.com>
2024-01-11 20:47:13 +0900

    part74完了

--- intermediate_sec12/src/components/molecules/MenuDrawer.tsx
+++ intermediate_sec12/src/components/molecules/MenuDrawer.tsx
@@ -10,18 +10,54 @@ import { FC, memo } from "react";
 type Props = {
   onClose: () => void;
   isOpen: boolean;
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
 };
 
 export const MenuDrawer: FC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const {
+    onClose,
+    isOpen,
+    onClickHome,
+    onClickUserManagement,
+    onClickSetting,
+  } = props;
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button w="100%">TOP</Button>
-            <Button w="100%">ユーザー一覧</Button>
-            <Button w="100%">設定</Button>
+            {/* <Button w="100%" onClick={onClickHome}> */}
+            <Button
+              w="100%"
+              onClick={() => {
+                onClickHome();
+                onClose();
+              }}
+            >
+              TOP
+            </Button>
+            {/* <Button w="100%" onClick={onClickUserManagement}> */}
+            <Button
+              w="100%"
+              onClick={() => {
+                onClickUserManagement();
+                onClose();
+              }}
+            >
+              ユーザー一覧
+            </Button>
+            {/* <Button w="100%" onClick={onClickSetting}> */}
+            <Button
+              w="100%"
+              onClick={() => {
+                onClickSetting();
+                onClose();
+              }}
+            >
+              設定
+            </Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
--- intermediate_sec12/src/components/organisms/layout/Header.tsx
+++ intermediate_sec12/src/components/organisms/layout/Header.tsx
@@ -1,10 +1,20 @@
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { FC, memo, useCallback } from "react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useNavigate } from "react-router-dom";
 
 export const Header: FC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+
+  const onClickHome = useCallback(() => navigate("/home"), []);
+  const onClickUserManagement = useCallback(
+    () => navigate("/home/user_management"),
+    []
+  );
+  const onClickSetting = useCallback(() => navigate("/home/setting"), []);
+
   return (
     <>
       <Flex
@@ -15,7 +25,13 @@ export const Header: FC = memo(() => {
         justify="space-between"
         padding={{ base: 3, md: 5 }}
       >
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex
+          align="center"
+          as="a"
+          mr={8}
+          _hover={{ cursor: "pointer" }}
+          onClick={onClickHome}
+        >
           <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
             ユーザー管理アプリ
           </Heading>
@@ -26,14 +42,20 @@ export const Header: FC = memo(() => {
           flexGrow={2}
           display={{ base: "none", md: "flex" }}
         >
-          <Box pr={4}>
+          <Box pr={4} onClick={onClickUserManagement}>
             <Link>ユーザー一覧</Link>
           </Box>
-          <Link>設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+      <MenuDrawer
+        onClose={onClose}
+        isOpen={isOpen}
+        onClickHome={onClickHome}
+        onClickUserManagement={onClickUserManagement}
+        onClickSetting={onClickSetting}
+      />
     </>
   );
 });
