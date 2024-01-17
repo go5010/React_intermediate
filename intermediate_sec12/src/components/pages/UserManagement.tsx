673bed05361a11a2fdced8eefede427db1a8da25
go5010 <gota.4305@gmail.com>
2024-01-17 20:37:30 +0900

    section12完了

--- intermediate_sec12/src/components/pages/UserManagement.tsx
+++ intermediate_sec12/src/components/pages/UserManagement.tsx
@@ -17,7 +17,6 @@ export const UserManagement: FC = memo(() => {
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
-  console.log(loginUser);
 
   useEffect(() => getUsers(), []);
 
@@ -49,7 +48,12 @@ export const UserManagement: FC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
