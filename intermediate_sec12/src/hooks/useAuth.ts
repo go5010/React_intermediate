b64f436fd2c3be9d3e9846ff8f9edea86863a9e4
go5010 <gota.4305@gmail.com>
2024-01-17 19:20:43 +0900

    part83完了

--- intermediate_sec12/src/hooks/useAuth.ts
+++ intermediate_sec12/src/hooks/useAuth.ts
@@ -4,10 +4,12 @@ import { useNavigate } from "react-router-dom";
 
 import { User } from "../types/api/user";
 import { useMessage } from "./useMessage";
+import { useLoginUser } from "./useLoginUser";
 
 export const useAuth = () => {
   const navigate = useNavigate();
   const { showMessage } = useMessage();
+  const { setLoginUser } = useLoginUser();
 
   const [loading, setLoading] = useState(false);
 
@@ -17,6 +19,7 @@ export const useAuth = () => {
       .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => {
         if (res.data) {
+          setLoginUser(res.data);
           showMessage({ title: "ログインしました", status: "success" });
           navigate("/home");
         } else {
